5a894dd16fd533a710cbccef885d5186e74fde89
subdragonzj <zj502407679@gmail.com>
2019-11-21 21:18:20 +0800

    grus: Define cne and dpm config
    Change-Id: I5aeab99201d31f7b05c943b30b0ee554f10e0558

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,6 +88,9 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Charger Mode
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
+# CNE and DPM
+BOARD_USES_QCNE := true
+
 #Enable HW based full disk encryption
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_HW_DISK_ENCRYPTION_PERF := true
