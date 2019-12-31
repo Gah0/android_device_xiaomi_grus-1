6dae0c32ceea1954eeae545d17251f715a5d87c7
Demon000 <demonsingur@gmail.com>
2019-12-31 01:06:45 +0100

    grus: fod: improve semantics
     * do not use (void) to silence unused parameters
     * do not specify namespace for IFingerprintInscreenCallback
    
    Change-Id: Ife9d0799a091c539028f775a728d2c8c31ffbf98

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -134,7 +134,7 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
     return Void();
 }
 
-Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t /* brightness */) {
     int realBrightness = get(BRIGHTNESS_PATH, 0);
     float alpha;
 
@@ -144,8 +144,6 @@ Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
         alpha = 1.0 - pow(realBrightness / 1680.0, 0.455);
     }
 
-    (void) brightness;
-
     return 255 * alpha;
 }
 
@@ -153,8 +151,7 @@ Return<bool> FingerprintInscreen::shouldBoostBrightness() {
     return false;
 }
 
-Return<void> FingerprintInscreen::setCallback(const sp<::vendor::mokee::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreenCallback>& callback) {
-    (void) callback;
+Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>& /* callback */) {
     return Void();
 }
 
